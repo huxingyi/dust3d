c0ef16cc6747848eb1e390d06e305f3ac14b0ace
Jeremy Hu <huxingyi@msn.com>
2017-12-17 22:01:50 +0800

    Change camera view

--- src/t_show.c
+++ src/t_show.c
@@ -4,75 +4,85 @@
 #include "dust3d.h"
 
 typedef struct cameraView {
-    float angle;
-    float directX;
-    float directZ;
-    float x;
-    float z;
+    float angleX;
+    float angleY;
+    float distance;
     dust3dState *state;
 } cameraView;
 
 static cameraView camera;
 
 static void initCamera(dust3dState *state) {
-    camera.angle = 0.0;
-    camera.directX = 0.0f;
-    camera.directZ = -1.0f;
-    camera.x = 0.0f;
-    camera.z = 10.0f;
+    camera.angleX = 30;
+    camera.angleY = -312;
+    camera.distance = 5.4;
     camera.state = state;
 }
 
 void onDisplay(void) {
     dust3dState *state = camera.state;
     
+    glClearColor(0.14, 0.14, 0.14, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(camera.x, 1.0f, camera.z,
-        camera.x + camera.directX, 1.0f,  camera.z + camera.directZ,
-        0.0f, 1.0f,  0.0f);
-
-    glColor3f(1.0f, 1.0f, 1.0f);
+    
+    glPushMatrix();
+    glTranslatef(0, 0, -camera.distance);
+    glRotatef(camera.angleX, 0, 1, 0);
+    glRotatef(camera.angleY, 1, 0, 0);
     {
         face *f;
         mesh *m = dust3dGetCurrentMesh(state);
+        glColor3f(1.0f, 1.0f, 1.0f);
         for (f = m->firstFace; f; f = f->next) {
             halfedge *it = f->handle;
             halfedge *stop = it;
             glBegin(GL_POLYGON);
             do {
                 glVertex3f(it->start->position.x,
-                    it->start->position.y,
-                    it->start->position.z);
+                    it->start->position.z,
+                    it->start->position.y);
                 it = it->next;
             } while (it != stop);
             glEnd();
         }
     }
+    {
+        face *f;
+        mesh *m = dust3dGetCurrentMesh(state);
+        glColor3f(0.07f, 0.07f, 0.07f);
+        for (f = m->firstFace; f; f = f->next) {
+            halfedge *it = f->handle;
+            halfedge *stop = it;
+            glBegin(GL_LINE_STRIP);
+            do {
+                glVertex3f(it->start->position.x,
+                    it->start->position.z,
+                    it->start->position.y);
+                it = it->next;
+            } while (it != stop);
+            glEnd();
+        }
+    }
+    glPopMatrix();
     
     glutSwapBuffers();
 }
 
 void onProcessSpecialKeys(int key, int x, int y) {
-    float fraction = 0.1f;
+    float fraction = 10;
     switch (key) {
     case GLUT_KEY_LEFT:
-        camera.angle -= 0.01f;
-        camera.directX = sin(camera.angle);
-        camera.directZ = -cos(camera.angle);
+        camera.angleX -= fraction;
         break;
     case GLUT_KEY_RIGHT:
-        camera.angle += 0.01f;
-        camera.directX = sin(camera.angle);
-        camera.directZ = -cos(camera.angle);
+        camera.angleX += fraction;
         break;
     case GLUT_KEY_UP:
-        camera.x += camera.directX * fraction;
-        camera.z += camera.directZ * fraction;
+        camera.angleY -= fraction;
         break;
     case GLUT_KEY_DOWN:
-        camera.x -= camera.directX * fraction;
-        camera.z -= camera.directZ * fraction;
+        camera.angleY += fraction;
         break;
     }
 }
@@ -83,13 +93,26 @@ void onResize(int w, int h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glViewport(0, 0, w, h);
-    gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+    gluPerspective(60.0f, ratio, 1, 1000);
     glMatrixMode(GL_MODELVIEW);
 }
 
 void onProcessNormalKeys(unsigned char key, int x, int y) {
+    float fraction = 1.0f;
     if (key == 27) {
         exit(0);
+    }
+    switch (key) {
+    case 'w':
+        camera.distance -= fraction;
+        break;
+    case 's':
+        camera.distance += fraction;
+        break;
+    case 'a':
+        break;
+    case 'd':
+        break;
     }
 }
 
