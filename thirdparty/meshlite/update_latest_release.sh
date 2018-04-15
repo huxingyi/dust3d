b7499199c818f6eae2cd061852ebdd988beb5a5a
Jeremy Hu <huxingyi@msn.com>
2018-04-15 23:17:09 +0800

    Add meshlite dll download script

---
+++ thirdparty/meshlite/update_latest_release.sh
@@ -0,0 +1,6 @@
+curl -L https://github.com/huxingyi/meshlite/releases/download/meshlite-unstable/meshlite_unstable_vc14_x86.zip --output meshlite_unstable_vc14_x86.zip
+rm -rf meshlite_unstable_vc14_x86/* && unzip meshlite_unstable_vc14_x86.zip -d meshlite_unstable_vc14_x86
+rm meshlite_unstable_vc14_x86.zip
+curl -L https://github.com/huxingyi/meshlite/releases/download/meshlite-unstable/meshlite_unstable_vc14_x64.zip --output meshlite_unstable_vc14_x64.zip
+rm -rf meshlite_unstable_vc14_x64/* && unzip meshlite_unstable_vc14_x64.zip -d meshlite_unstable_vc14_x64
+rm meshlite_unstable_vc14_x64.zip
