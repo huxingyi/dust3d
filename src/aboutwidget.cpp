2156855444ea9b6c89fde81386fe64e26616eca0
Jeremy Hu <huxingyi@msn.com>
2018-09-23 09:02:08 +0800

    Add show if opengl core profile used in about widget

--- src/aboutwidget.cpp
+++ src/aboutwidget.cpp
@@ -8,7 +8,7 @@
 AboutWidget::AboutWidget()
 {
     QTextEdit *versionInfoLabel = new QTextEdit;
-    versionInfoLabel->setText(QString("%1 %2 (version: %3 build: %4 %5)\nopengl: %6 shader: %7").arg(APP_NAME).arg(APP_HUMAN_VER).arg(APP_VER).arg(__DATE__).arg(__TIME__).arg((char *)glGetString(GL_VERSION)).arg((char *)glGetString(GL_SHADING_LANGUAGE_VERSION)));
+    versionInfoLabel->setText(QString("%1 %2 (version: %3 build: %4 %5)\nopengl: %6 shader: %7 core: %8").arg(APP_NAME).arg(APP_HUMAN_VER).arg(APP_VER).arg(__DATE__).arg(__TIME__).arg((char *)glGetString(GL_VERSION)).arg((char *)glGetString(GL_SHADING_LANGUAGE_VERSION)).arg(QSurfaceFormat::defaultFormat().profile() == QSurfaceFormat::CoreProfile ? "true" : "false"));
     versionInfoLabel->setReadOnly(true);
     
     QVBoxLayout *mainLayout = new QVBoxLayout;
