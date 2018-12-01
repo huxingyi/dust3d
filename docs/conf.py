82a28ff1f2a9973c610e7c2e0dcf339e7e69e8b9
Jeremy Hu <huxingyi@msn.com>
2018-12-01 10:59:33 +0930

    Change docs version to 1.0.0-beta

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'Xingyi HU'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'0.0-alpha1'
+release = u'1.0.0-beta'
 
 
 # -- General configuration ---------------------------------------------------
