66c10ec5c39e0bfb907a5bdfd5661ebd6e6f6703
huxingyi <huxingyi@msn.com>
2020-10-22 07:02:15 +0930

    Update color for example screwdriver model

--- src/rigwidget.cpp
+++ src/rigwidget.cpp
@@ -36,6 +36,7 @@ RigWidget::RigWidget(const Document *document, QWidget *parent) :
     m_rigWeightRenderWidget->setXRotation(0);
     m_rigWeightRenderWidget->setYRotation(0);
     m_rigWeightRenderWidget->setZRotation(0);
+    m_rigWeightRenderWidget->setEyePosition(QVector3D(0.0, 0.0, -2.0));
     m_rigWeightRenderWidget->toggleWireframe();
     
     m_infoLabel = new InfoLabel;
@@ -43,6 +44,7 @@ RigWidget::RigWidget(const Document *document, QWidget *parent) :
     
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addLayout(formLayout);
+    layout->addSpacing(10);
     layout->addWidget(m_rigWeightRenderWidget);
     layout->addWidget(m_infoLabel);
     layout->addStretch();
