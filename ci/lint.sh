9cc04a36d679d696c0dcf79cb4b06231e126c46d
Jeremy HU <huxingyi@msn.com>
2022-12-12 19:49:37 +1100

    Use fixed .clang-format

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,2 +1,2 @@
-# sudo apt install clang-format
-find ../application/sources/ ../dust3d/ -iname *.h -o -iname *.cc | xargs clang-format -style=webkit -i
+# When using -style=file, clang-format uses the .clang-format file in the closest parent directory of the input file.
+find ../application/sources/ ../dust3d/ -iname *.h -o -iname *.cc | xargs clang-format -style=file -i
