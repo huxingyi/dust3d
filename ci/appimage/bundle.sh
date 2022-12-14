92284b79790ed0f1247e8159879014b518dc6b12
Jeremy HU <huxingyi@msn.com>
2022-12-14 23:13:21 +1100

    Fix pipeline

--- ci/appimage/bundle.sh
+++ ci/appimage/bundle.sh
@@ -2,18 +2,42 @@
 set -e
 
 # Download appimage tools
-wget --no-verbose -O ./ci/appimage/linuxdeployqt-continuous-x86_64.AppImage https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
-chmod a+x ./ci/appimage/linuxdeployqt-continuous-x86_64.AppImage
+wget --no-verbose -O ./ci/appimage/AppRun-patched-x86_64 https://github.com/huxingyi/dust3d/blob/1.0.0-rc.6/ci/AppRun-patched-x86_64?raw=true
+wget --no-verbose -O ./ci/appimage/exec-x86_64.so https://github.com/huxingyi/dust3d/blob/1.0.0-rc.6/ci/exec-x86_64.so?raw=true
+wget --no-verbose -O ./ci/appimage/linuxdeployqt.AppImage https://github.com/huxingyi/dust3d/blob/1.0.0-rc.6/ci/linuxdeployqt.AppImage?raw=true
 
-# Create appdir directories
+# Create directories
+mkdir -p appdir/usr/share/metainfo
 mkdir -p appdir/usr/share/applications
 mkdir -p appdir/usr/bin
+mkdir -p appdir/usr/optional/libstdc++
+
+# Print GLIBC version
+ldd --version
+
+# Print libstdc related
+sudo ls /usr/lib/x86_64-linux-gnu/ | grep libstdc
+
+# Copy libstdc++
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6 appdir/usr/optional/libstdc++/libstdc++.so.6
+
+# Copy exec
+cp ./ci/appimage/exec-x86_64.so appdir/usr/optional/exec.so
 
 # Copy application related files
 cp ./ci/appimage/dust3d.png appdir/dust3d.png
+cp ./ci/appimage/dust3d.appdata.xml appdir/usr/share/metainfo/dust3d.appdata.xml
 cp ./ci/appimage/dust3d.desktop appdir/usr/share/applications/dust3d.desktop
 cp ./application/dust3d appdir/usr/bin/dust3d
 
 # Make bundle
-./ci/appimage/linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
-mv Dust3D-*-x86_64.AppImage Dust3D-x86_64.AppImage
+chmod a+x ./ci/appimage/linuxdeployqt.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH 
+./ci/appimage/linuxdeployqt.AppImage appdir/usr/share/applications/dust3d.desktop -bundle-non-qt-libs -verbose=2
+rm appdir/AppRun
+cp ./ci/appimage/AppRun-patched-x86_64 appdir/AppRun
+chmod a+x appdir/AppRun
+./ci/appimage/linuxdeployqt.AppImage --appimage-extract
+export PATH=$(readlink -f ./squashfs-root/usr/bin):$PATH
+rm -f "./appdir/usr/lib/libxcb-dri2.so" "./appdir/usr/lib/libxcb-dri3.so"
+./squashfs-root/usr/bin/appimagetool -g ./appdir/ Dust3D-x86_64.AppImage
